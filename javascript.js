6351bf71fdb00cd0729dee0be9eb53f26fb1faaf
francescacosta <francescacosta@Francescas-iMac-2.local>
2017-06-03 21:44:00 +1000

    creating online and offline buttons

--- javascript.js
+++ javascript.js
@@ -18,34 +18,34 @@ channels.forEach(function(name) {
       var row = `<td><img src="${urls[name]}" /></td>`
       row += `<td><a href="${url}" target="_blank">${name}</a></td>`
       row += `<td>Offline</td>`
+      channelData.insertAdjacentHTML('beforeend', `<tr class="offline">${row}</tr>`)
     } else {
       var row = `<td><img src="${data.stream.channel.logo}" /></td>`
       row += `<td><a href="${url}" target="_blank">${name}</a></td>`
       row += `<td>${data.stream.channel.status}</td>`
+      channelData.insertAdjacentHTML('beforeend', `<tr class="online">${row}</tr>`)
     }
 
-    channelData.insertAdjacentHTML('beforeend', `<tr>${row}</tr>`)
-
-    // <table>
-    //   <tbody>
-    //     <tr>
-    //       <td>logo</td>
-    //       <td>channel name</td>
-    //       <td>status</td>
-    //     </tr>
-    //   <tbody>
-    // </table>
 
 
-    //console.log(data.stream)
   });
-
 
 });
 
+var showOnline = document.getElementsByClassName("show-online")[0]
 
+showOnline.onclick = function() {
+  var hideOffline = document.getElementsByClassName('offline')
+  for (i = 0; i < hideOffline.length; i++) {
+    hideOffline[i].classList = 'offline hide'
+  }
+}
 
+var showOffline = document.getElementsByClassName("show-offline")[0]
 
-// 2. put all the channels into my tv container with an image and styling
-// 4. Click on online, get data from JSON about which of the current channels are online
-// Click on offline, get data from JSON about which of the current channels are offline //
+showOffline.onclick = function() {
+  var hideOnline = document.getElementsByClassName('online')
+  for (i = 0; i < hideOnline.length; i++) {
+    hideOnline[i].classList = 'online hide'
+  }
+}
